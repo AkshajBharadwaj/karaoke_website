31d765dee6e08997c9942a753576104d9b8827d4
akshajb <akshajb@uw.edu>
2025-04-14 21:49:40 -0700

    direct github should work

--- youtube-audio-downloader/server.js
+++ youtube-audio-downloader/server.js
@@ -43,7 +43,7 @@ app.post("/karaokeify", async (req, res) => {
 
   try {
     // Construct command
-    const downloadCmd = `/usr/local/bin/yt-dlp --ffmpeg-location /usr/bin/ffmpeg -f bestaudio -x --audio-format mp3 -o "${inputFile}" "${url}"`;
+  const downloadCmd = `/usr/local/bin/yt-dlp --ffmpeg-location /usr/bin/ffmpeg -f bestaudio -x --audio-format mp3 -o "${inputFile}" "${url}"`;
 
     console.log("⏬ Running yt-dlp command:", downloadCmd);
     exec("ls -l /usr/local/bin/yt-dlp", (err, stdout, stderr) => {
