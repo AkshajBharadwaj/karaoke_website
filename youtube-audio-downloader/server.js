8f86ab280f64dd73790d622ba98459997a302aa2
akshajb <akshajb@uw.edu>
2025-04-14 19:17:29 -0700

    changes to git

--- youtube-audio-downloader/server.js
+++ youtube-audio-downloader/server.js
@@ -43,7 +43,7 @@ app.post("/karaokeify", async (req, res) => {
 
   try {
     // Construct command
-    const downloadCmd = `/root/.local/bin/yt-dlp --ffmpeg-location /usr/bin/ffmpeg -f bestaudio -x --audio-format mp3 -o "${inputFile}" "${url}"`;
+    const downloadCmd = `/usr/bin/yt-dlp --ffmpeg-location /usr/bin/ffmpeg -f bestaudio -x --audio-format mp3 -o "${inputFile}" "${url}"`;
 
     console.log("⏬ Running yt-dlp command:", downloadCmd);
 
